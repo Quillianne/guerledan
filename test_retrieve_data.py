b2ab6dc16c3897bbd89517024e746ac28ca1fa8f
Quillianne <kili@nbarantal.me>
2024-09-09 15:54:41 +0200

    test

--- test_retrieve_data.py
+++ test_retrieve_data.py
@@ -1,6 +1,17 @@
 import sys
 import os
+import time
 sys.path.append(os.path.join(os.path.dirname(__file__), 'drivers-ddboat-v2'))
 
 import imu9_driver_v2 as imudrv
 imu = imudrv.Imu9IO()
+
+def print_data(sensor):
+    print("MAG: " + str(sensor.read_mag_raw()))
+    print("ACC: " + str(sensor.read_accel_raw()))
+    print("GYRO: " + str(sensor.read_gyro_raw()))
+
+while True:
+    os.system("clear")
+    print_data(imu)
+    time.sleep(0.1)
