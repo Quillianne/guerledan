2a6a6a626aac8c321e3cf2dfc5efee940f4a7194
aureleplan <aurele.plan@outlook.fr>
2024-09-11 10:27:17 +0200

    dfvgqd

--- DDGOATlib.py
+++ DDGOATlib.py
@@ -255,14 +255,14 @@ def get_point_boat():
     """
     # coordonnées gps (degrés)
     point_gps = get_gps()
-    x, y = conversion_spherique_cartesien(point_gps, point de réf qu on a pas encore)
+    x, y = conversion_spherique_cartesien(point_gps, !!!point de réf qu on a pas encore!!!)
 
     return x, y
 
     return x,y
 
 def vecteur_d(position, objectif, vitesse_objectif,ordre_de_grandeur=5): #fonction avec la tan_hyperbolique,etc...
-    pass
+    
 
     return x,y
 
