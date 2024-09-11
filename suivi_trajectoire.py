f635a73f044136f0be285d7fdbf95f4655ffb015
aureleplan <aurele.plan@outlook.fr>
2024-09-11 11:40:06 +0200

    fichier suivi traj

---
+++ suivi_trajectoire.py
@@ -0,0 +1,7 @@
+from DDGOATlib import suivi_gps, declenchement, suivi_trajectoire, lissajou, lissajou_point
+import time
+
+declenchement()
+suivi_trajectoire(lissajou, lissajou_point)
+time.sleep(10)
+suivi_gps((48.1992385, -3.0147241))
