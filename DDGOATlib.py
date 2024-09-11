519a9941e6e6fddc53ae15e5e52dca175cbf336f
Quillianne <74460699+Quillianne@users.noreply.github.com>
2024-09-11 12:25:49 +0200

    Update DDGOATlib.py

--- DDGOATlib.py
+++ DDGOATlib.py
@@ -277,7 +277,7 @@ def vecteur_d(position:np.array, objectif:np.array, vitesse_objectif:np.array, o
     # erreur : vecteur entre les 2 points
     e = objectif - position
     e_norm = np.linalg.norm(e)
-    d = -(Kp * e/e_norm * np.tanh(e_norm/ordre_de_grandeur) + vitesse_objectif/10)
+    d = (Kp * e/e_norm * np.tanh(e_norm/ordre_de_grandeur) + vitesse_objectif/10)
     print(d)
     return d
 
@@ -293,7 +293,7 @@ def suivi_trajectoire(fonction, fonction_derive): #fonction qui suit la trajecto
         if coord_boat != None:
 
             vecteur = vecteur_d(coord_boat, obj, vitesse_obj)
-            cap = np.arctan2(vecteur[1],vecteur[0])*180/np.pi
+            cap = -np.arctan2(vecteur[1],vecteur[0])*180/np.pi
             print(cap)
             vitesse = min(15*np.linalg.norm(vecteur),255)
             suivi_cap(cap, duree = 0.2, spd_base = vitesse)
