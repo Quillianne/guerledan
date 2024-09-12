0158c861f4563783770f49e1ed4a9770fbefc384
Moi <noe.lemaistre@ensta-bretagne.org>
2024-09-12 15:15:36 +0200

    descamps 2

--- DDGOATlib.py
+++ DDGOATlib.py
@@ -534,7 +534,7 @@ def cap_chemin(p, m=[48.1996872, -3.0153766], A=[48.1996457, -3.0152944]):
     vect_mA = vect_mA/np.linalg.norm(vect_mA)
 
     # Cap de la ligne (angle entre la ligne et l'axe x)
-    chemin = -np.arctan2(vect_mA[1], vect_mA[0])
+    chemin = np.arctan2(vect_mA[1], vect_mA[0]) + np.pi
     print('le chemin est', (chemin*180/np.pi))
     # Calcul de la distance perpendiculaire du point p à la droite définie par (m, A)
     distance = np.cross(vect_mA, p_car - m_car) / np.linalg.norm(vect_mA)
