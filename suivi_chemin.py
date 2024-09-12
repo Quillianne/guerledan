5c032935f2f71c52affdc1424754c836acd178ce
aureleplan <aurele.plan@outlook.fr>
2024-09-12 11:41:10 +0200

    ezgzs

--- suivi_chemin.py
+++ suivi_chemin.py
@@ -2,7 +2,7 @@ from DDGOATlib import declenchement, suivi_gps, suivi_chemin_temps
 import time
 
 
-declenchement()
+#declenchement()
 suivi_chemin_temps()
 time.sleep(10)
 suivi_gps((48.1992385, -3.0147241))
