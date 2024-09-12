7af0264964d9e0ed79a9444fe9fd87066cd12e1d
aureleplan <aurele.plan@outlook.fr>
2024-09-12 14:34:16 +0200

    coordonées

--- DDGOATlib.py
+++ DDGOATlib.py
@@ -505,8 +505,8 @@ def suivi_trajectoire(fonction, fonction_derive,duree=300, Kp_cap=2, Kp_vitesse=
 # coordonnées GPS des points importants :
 point_M = (48.1996872, -3.0153766)
 point_A = (48.1996457, -3.0152944)
-point_B = ()
-point_C = ()
+point_B = (48.2008333, -3.0163889)
+point_C = (48.2019444, -3.0147222)
 
 
 def cap_chemin(p, m=[48.1996872, -3.0153766], A=[48.1996457, -3.0152944]):
